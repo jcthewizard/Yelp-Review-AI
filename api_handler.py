12a5416bb0d1518c0513b56c430efdffdf65e8bb
“Bugs <joshchou@uw.edu>
2023-12-29 14:56:16 -0800

    Working but with old/bad model

--- api_handler.py
+++ api_handler.py
@@ -1,28 +1,23 @@
 # Contains the logic for interacting with the OpenAI GPT API
-import requests
+import openai
+from dotenv import load_dotenv
 import os
 
-api_key = os.getenv('YELP_API_KEY')
-headers = {'Authorization': f'Bearer {api_key}'}
-response = requests.get('https://api.yelp.com/v3/businesses/search', headers=headers)
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
+def summarize_reviews_with_gpt(concatenated_reviews):
+    prompt = f"Please summarize the following restaurant reviews: {concatenated_reviews} please outline the things that customers seem to enjoy and dislike. please try to touch on the subjects of ambiance, price, location, taste if possible. Also try to highlight dishes that were favorites if possible."
 
-def get_restaurant_details(restaurant_id, api_key):
-    """Fetch details for a specific restaurant using the Yelp API."""
-    url = f"https://api.yelp.com/v3/businesses/{restaurant_id}"
-    headers = {'Authorization': f'Bearer {api_key}'}
-    response = requests.get(url, headers=headers)
-    return response.json()
+    try:
+        response = openai.Completion.create(
+            engine="davinci",  # Replace with the latest model name
+            prompt=prompt,
+            # max_tokens=1000  # Adjust as needed
+        )
+        summary = response.choices[0].text.strip()
+        return summary
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-def get_restaurant_reviews(restaurant_id, api_key):
-    """Fetch reviews for a specific restaurant using the Yelp API."""
-    url = f"https://api.yelp.com/v3/businesses/{restaurant_id}/reviews"
-    headers = {'Authorization': f'Bearer {api_key}'}
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-if __name__ == "__main__":
-    api_key = "your_api_key"
-    restaurant_id = "example_restaurant_id"
-    print(get_restaurant_details(restaurant_id, api_key))
-    print(get_restaurant_reviews(restaurant_id, api_key))
--- yelp_handler.py
+++ yelp_handler.py
@@ -1,1 +1,58 @@
 # Manages the interactions with the Yelp API.
+import requests
+import os
+from dotenv import load_dotenv
+from api_handler import summarize_reviews_with_gpt
+
+# Load environment variables from .env file
+load_dotenv()
+api_key = os.getenv('YELP_API_KEY')
+headers = {'Authorization': f'Bearer {api_key}'}
+response = requests.get('https://api.yelp.com/v3/businesses/search', headers=headers)
+
+# Get Restaurant Details
+# def get_restaurant_details(restaurant_id, api_key):
+#     """Fetch details for a specific restaurant using the Yelp API."""
+#     url = f"https://api.yelp.com/v3/businesses/{restaurant_id}"
+#     headers = {'Authorization': f'Bearer {api_key}'}
+#     response = requests.get(url, headers=headers)
+#     return response.json()
+
+def get_restaurant_reviews(restaurant_id, api_key):
+    """Fetch reviews for a specific restaurant using the Yelp API."""
+    url = f"https://api.yelp.com/v3/businesses/{restaurant_id}/reviews"
+    headers = {'Authorization': f'Bearer {api_key}'}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+# Test the routes
+# if __name__ == "__main__":
+#     api_key = os.getenv('YELP_API_KEY')
+#     restaurant_id = "north-india-restaurant-san-francisco"
+#     #print(get_restaurant_details(restaurant_id, api_key))
+#     print(get_restaurant_reviews(restaurant_id, api_key))
+
+def extract_review_texts(yelp_response):
+    # print(yelp_response)
+    reviews = []
+    for review in yelp_response['reviews']:
+        reviews.append(review['text'])
+    return reviews
+
+def concatenate_reviews(review_texts):
+    # Join the reviews using a separator (like a newline or period)
+    concatenated_reviews = " ".join(review_texts)
+    return concatenated_reviews
+
+
+restaurant_id = "north-india-restaurant-san-francisco"
+# print(extract_review_texts(get_restaurant_reviews(restaurant_id, api_key)))
+# Extract review texts
+review_texts = extract_review_texts(get_restaurant_reviews(restaurant_id, api_key))
+
+# # Concatenate reviews
+concatenated_reviews = concatenate_reviews(review_texts)
+
+# Summarize with GPT API
+summary = summarize_reviews_with_gpt(concatenated_reviews)
+print(summary)
