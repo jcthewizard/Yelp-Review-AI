fc0349c946bc916b8fa67f05271edf1a6ded9d21
“Bugs <joshchou@uw.edu>
2023-12-29 15:23:32 -0800

    Works with gpt3.5 turbo!

--- api_handler.py
+++ api_handler.py
@@ -5,17 +5,20 @@ import os
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
+message_history = []
 
 def summarize_reviews_with_gpt(concatenated_reviews):
     prompt = f"Please summarize the following restaurant reviews: {concatenated_reviews} please outline the things that customers seem to enjoy and dislike. please try to touch on the subjects of ambiance, price, location, taste if possible. Also try to highlight dishes that were favorites if possible."
-
+    message_history.append({"role": "user", "content": prompt})
     try:
-        response = openai.Completion.create(
-            engine="davinci",  # Replace with the latest model name
-            prompt=prompt,
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # Replace with the latest model name
+            messages=message_history,
             # max_tokens=1000  # Adjust as needed
         )
-        summary = response.choices[0].text.strip()
+        summary = response.choices[0].message.content
+        message_history.append({"role": "assistant", "content": summary})
+
         return summary
     except Exception as e:
         print(f"An error occurred: {e}")
